d8416bd24732a59b1b336e92af73d6fe34a7b313
Zied ZOUAOUI <zzouaoui@gmail.com>
2018-03-19 01:17:33 +0100

    commit 4

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -38,7 +38,7 @@ public class Calculatrice extends JFrame {
         	
                 
                 this.setSize(350, 300); //(Largeur, Hauteur)
-                this.setTitle("Calculatricetttyyy"); //Nom programme
+                this.setTitle("Calculatricetttyyyhh"); //Nom programme
                 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 this.setLocationRelativeTo(null);
                 this.setResizable(false);
